cd5d6aeef66b63c0685b4168d612cddfbd80a8c8
Paulo Henrique <ph.pereira.7@gmail.com>
2024-10-02 17:05:10 -0300

    Protótipo de tela de cadastro

--- lib/views/newaccount.page.dart
+++ lib/views/newaccount.page.dart
@@ -1,109 +1,146 @@
+// ignore_for_file: prefer_const_constructors
 
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
-
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class NewAccountPage extends StatelessWidget {
-  
+  const NewAccountPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("New Account"),
+        // title: Text("Nova Conta"),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  margin: EdgeInsets.only(bottom: 32),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF7F7FC),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Stack(
-                    children: [
-                      Center(child: Icon(Icons.person_outline, size: 56)),
-                      Positioned(
-                        child: Icon(Icons.add_circle, size: 24),
-                        bottom: 0,
-                        right: 1,
-                      ),
-                    ],
-                  )
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF7F7FC),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      hintText: "Name",
-                      hintStyle: TextStyle(),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 12),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF7F7FC),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: "E-mail",
-                      hintStyle: TextStyle(),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 12),
-                Container(
-                  color: Color(0xFFF7F7FC),
-                  padding: EdgeInsets.all(8),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      hintStyle: TextStyle(),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Color(0xFF002DE3)),
-                    ),
-                    child: Text(
-                      "Save",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Olá! Novo por aqui?',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 20
+              ),
             ),
-          ),
+            Text(
+              'Qual será seu tipo de conta?',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 20
+              ),
+            ),
+            SizedBox(height: 12),
+            AccountTypeRadioButton(),
+            SizedBox(height: 12),
+            Container(
+              padding: EdgeInsets.all(7), // Tamanho do campo
+              width: 360,
+              decoration: BoxDecoration(
+                color: Color(0xFFF7F7FC),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: const Color(0xFF565656), // Cor do contorno
+                  width: 1.5, // Largura do contorno
+                ),
+              ),
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: "Nome",
+                  hintStyle: TextStyle(),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                )
+              ),
+            ),
+            SizedBox(height: 12),
+            Container(
+              padding: EdgeInsets.all(7), // Tamanho do campo
+              width: 360,
+              decoration: BoxDecoration(
+                color: Color(0xFFF7F7FC),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: const Color(0xFF565656), // Cor do contorno
+                  width: 1.5, // Largura do contorno
+                ),
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "E-mail",
+                  hintStyle: TextStyle(),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            Container(
+              padding: EdgeInsets.all(7), // Tamanho do campo
+              width: 360,
+              decoration: BoxDecoration(
+                color: Color(0xFFF7F7FC),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: const Color(0xFF565656), // Cor do contorno
+                  width: 1.5, // Largura do contorno
+                ),
+              ),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Senha",
+                  hintStyle: TextStyle(),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
+      )
+    );
+  }
+}
+
+enum AccountType { solicitante, aluno }
+
+class AccountTypeRadioButton extends StatefulWidget {
+  const AccountTypeRadioButton({super.key});
+
+  @override
+  State<AccountTypeRadioButton> createState() => _AccountTypeRadioButtonState();
+}
+
+class _AccountTypeRadioButtonState extends State<AccountTypeRadioButton> {
+  AccountType? _type = AccountType.solicitante;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+         RadioListTile<AccountType>(
+          title: Text('Solicitante'), 
+          value: AccountType.solicitante, 
+          groupValue: _type,
+          onChanged: (AccountType? value) {
+            setState(() {
+              _type = value;
+            });
+          },
+        ),
+        RadioListTile<AccountType>(
+          title: Text('Aluno'), 
+          value: AccountType.aluno, 
+          groupValue: _type,
+          onChanged: (AccountType? value) {
+            setState(() {
+              _type = value;
+            });
+          },
+        )
+      ],
     );
   }
 }
