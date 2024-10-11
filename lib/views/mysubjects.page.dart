e3738fca5714502f67413042b059b6156ba4d8fa
Geovanna <geovanna.roma@hotmail.com>
2024-10-10 22:40:18 -0300

    Design dos Meus Assuntos atualizado

--- lib/views/mysubjects.page.dart
+++ lib/views/mysubjects.page.dart
@@ -38,37 +38,85 @@ class MySubjects extends StatelessWidget {
                     color: Color(0xFF3D3D3D),
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.logout_rounded, color: Color(0xFF3D3D3D)),
-                  onPressed: () {
-                    // Navigator.of(context).pop(); 
-                  },
+
+                Container(
+                  padding: EdgeInsets.all(2), // Tamanho do círculo
+                  decoration: BoxDecoration(
+                    color: Color(0xFF4DC09E),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.logout_rounded, color: Color(0xFF13715B)),
+                    onPressed: () {
+                      // Navigator.of(context).pop();
+                      Navigator.pushNamed(context, '/login');
+                    },
+                  ),
                 ),
               ],
             ),
           ),
         ),
       ),
+
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20),
+        margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
         decoration: BoxDecoration(
           border: Border.all(
-            color: Colors.black, // Cor da borda
-            width: 3,          // Largura da borda
+            color: Colors.grey, // Cor da borda
+            width: 1,          // Largura da borda
           ),
-          borderRadius: BorderRadius.circular(12), // Opcional: para bordas arredondadas
+          borderRadius: BorderRadius.circular(22), // Opcional: para bordas arredondadas
         ),
         child: ListView.builder(
           itemCount: 20,
           itemBuilder: (context, index) {
-            return Center( // Centraliza o conteúdo do ListTile
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8, // Define uma largura customizada
-                child: ListTile(
-                  title: Center(child: Text('Um assunto qualquer')),
-                  onTap: () => Navigator.pushNamed(context, '/subject'),
+            return Column(
+              children: [
+                Center( // Centraliza o conteúdo do ListTile
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    width: MediaQuery.of(context).size.width * 0.8, // Define uma largura customizada
+                    child: ListTile(
+                      title: Center(
+                        child: Text(
+                          'Um assunto qualquer',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFF13715B),
+                          ),
+                        )
+                       ),
+
+                       subtitle: Center(
+                         child: Container(
+                          margin: EdgeInsets.only(top: 15),
+                           child: Text(
+                              'Descrição do assunto...',
+                              style: TextStyle(
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                                fontSize: 19,
+                                color: Color.fromARGB(255, 74, 74, 74),
+                              ),
+                            ),
+                         ),
+                       ),
+                      onTap: () => Navigator.pushNamed(context, '/subject'),
+                    ),
+                  ),
                 ),
-              ),
+
+                Divider(
+                  color: Colors.grey,
+                  thickness: 1, // Espessura da linha
+                  indent: 70,   // Espaçamento à esquerda
+                  endIndent: 70, // Espaçamento à direita
+                ),
+                
+              ],
             );
           }
         ),
