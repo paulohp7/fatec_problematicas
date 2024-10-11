0de1cbfdf20a8a8d3a6c8bf34302e27a8c9caa16
Geovanna <geovanna.roma@hotmail.com>
2024-10-11 01:44:55 -0300

    Design da Busca de Assunto

--- lib/views/searchsubjects.page.dart
+++ lib/views/searchsubjects.page.dart
@@ -48,26 +48,57 @@ class SearchSubjects extends StatelessWidget {
                     color: Color(0xFF3D3D3D),
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.logout_rounded, color: Color(0xFF3D3D3D)),
-                  onPressed: () {
-                    // Navigator.of(context).pop(); 
-                  },
+
+                Container(
+                  padding: EdgeInsets.all(2), // Tamanho do círculo
+                  decoration: BoxDecoration(
+                    color: Color(0xFF4DC09E),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.logout_rounded, color: Color(0xFF13715B)),
+                    onPressed: () {
+                      // Navigator.of(context).pop();
+                      Navigator.pushNamed(context, '/login');
+                    },
+                  ),
                 ),
               ],
             ),
           ),
         ),
       ),
+
       body: Column(
         children: [
+
+          // --------- Campo de Busca ----------
           Container(
+            margin: EdgeInsets.only(top: 20),
             width: 350,
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Color(0xFFF7F7FC),
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                color: const Color(0xFF565656), // Cor do contorno
+                width: 1.5, // Largura do contorno
+              ),
+            ),
             child: TextField(
+              style: TextStyle(
+                fontFamily: 'Inter', // estilo do texto digitado no campo
+                fontSize: 18,
+              ),
               decoration: InputDecoration(
                 hintText: "Digite sua busca",
-                border: OutlineInputBorder(),
+                hintStyle: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 17,
+                  color: Color(0xFF696969)
+                ),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
                 suffixIcon: Icon(Icons.search_rounded)
               ),
             ),
@@ -75,27 +106,73 @@ class SearchSubjects extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+
+              // --------- Botão de aplicar filtros ----------
               Container(
                 padding: EdgeInsets.all(20),
                 child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10), // Borda circular
+                    ),
+                    minimumSize: Size(20, 50),
+                    backgroundColor: Color(0xFF4DC09E),
+                  ),
                   onPressed: () => applyFilter(context), 
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Aplicar Filtros'),
-                      Icon(Icons.filter_list_rounded)
+                      Text(
+                        'Aplicar Filtros',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: const Color(0xFF216655),
+                        ),
+                      ),
+
+                      SizedBox(width: 10),
+
+                      Icon(
+                        Icons.filter_list_rounded,
+                        color: Color(0xFF216655),
+                      )
                     ],
                   )),
               ),
+
+              // --------- Botão de limpar filtros ----------
               Container(
                 padding: EdgeInsets.all(20),
                 child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10), // Borda circular
+                    ),
+                    minimumSize: Size(20, 50),
+                    backgroundColor: Color(0xFFB8D9CF),
+                  ),
                   onPressed: () => {}, 
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Limpar Filtros'),
-                      Icon(Icons.filter_list_off_rounded)
+                      Text(
+                        'Limpar Filtros',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: const Color(0xFF287A65),
+                        ),
+                      ),
+
+                      SizedBox(width: 10),
+
+                      Icon(
+                        Icons.filter_list_off_rounded,
+                        color: const Color(0xFF287A65),
+                      )
                     ],
                   )),
               ),
@@ -103,25 +180,63 @@ class SearchSubjects extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
+              margin: EdgeInsets.only(left: 45, right: 45, top: 10, bottom: 50),
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.black, // Cor da borda
-                  width: 3,          // Largura da borda
+                  color: Colors.grey, // Cor da borda
+                  width: 1,          // Largura da borda
                 ),
-                borderRadius: BorderRadius.circular(12), // Opcional: para bordas arredondadas
+                borderRadius: BorderRadius.circular(22), // Opcional: para bordas arredondadas
               ),
               child: ListView.builder(
                 itemCount: 20,
                 itemBuilder: (context, index) {
-                  return Center( // Centraliza o conteúdo do ListTile
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.8, // Define uma largura customizada
-                      child: ListTile(
-                        title: Center(child: Text('Um assunto qualquer')),
-                        onTap: () => Navigator.pushNamed(context, '/subject'),
+                  return Column(
+                    children: [
+                      Center( // Centraliza o conteúdo do ListTile
+                        child: Container(
+                          margin: EdgeInsets.all(15),
+                          width: MediaQuery.of(context).size.width * 0.8, // Define uma largura customizada
+                          child: ListTile(
+                            title: Center(
+                              child: Text(
+                                'Um assunto qualquer',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                  color: Color(0xFF13715B),
+                                ),
+                              )
+                            ),
+
+                            subtitle: Center(
+                              child: Container(
+                                margin: EdgeInsets.only(top: 15),
+                                child: Text(
+                                    'Descrição do assunto...',
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 19,
+                                      color: Color.fromARGB(255, 74, 74, 74),
+                                    ),
+                                  ),
+                              ),
+                            ),
+                            onTap: () => Navigator.pushNamed(context, '/subject'),
+                          ),
+                        ),
                       ),
-                    ),
+
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 1, // Espessura da linha
+                        indent: 70,   // Espaçamento à esquerda
+                        endIndent: 70, // Espaçamento à direita
+                      ),
+                      
+                    ],
                   );
                 }
               ),
